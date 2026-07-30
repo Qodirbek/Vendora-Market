49a3dea188a5d8a3ee65d1c4e8ae0aa00a33e0dc
Qodirbek <qodirbekpubg13@gmail.com>
2026-07-30 22:31:40 +0500

    Update SQLALCHEMY_DATABASE_URI for PostgreSQL support

--- config.py
+++ config.py
@@ -13,7 +13,18 @@ class Config:
 
     SECRET_KEY = "secret-key"
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database.db")
+    import os
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
+SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
