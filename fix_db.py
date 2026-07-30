dabe3d1ac62bb578fbbcca3cabd16e83b4c87f3f
Qodirbek <qodirbekpubg13@gmail.com>
2026-07-30 23:34:06 +0500

    Update fix_db.py

--- fix_db.py
+++ fix_db.py
@@ -6,6 +6,7 @@ from sqlalchemy import text
 with app.app_context():
 
     columns = [
+
         """
         ALTER TABLE "user"
         ADD COLUMN IF NOT EXISTS allow_cash BOOLEAN DEFAULT TRUE;
@@ -14,14 +15,21 @@ with app.app_context():
         """
         ALTER TABLE "user"
         ADD COLUMN IF NOT EXISTS allow_card BOOLEAN DEFAULT TRUE;
+        """,
+
         """
+        ALTER TABLE "user"
+        ADD COLUMN IF NOT EXISTS is_blocked BOOLEAN DEFAULT FALSE;
+        """
+
     ]
 
 
-    for sql in columns:
-        db.session.execute(text(sql))
+    for column in columns:
+        db.session.execute(text(column))
 
 
     db.session.commit()
 
-    print("✅ USER columns fixed")
+
+print("✅ USER columns fixed")
