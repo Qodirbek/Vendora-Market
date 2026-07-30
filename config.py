f21173faa6104ad784f1323f6b754405ec4c4720
Qodirbek <qodirbekpubg13@gmail.com>
2026-07-30 22:49:58 +0500

    Refactor config.py for environment variable support
    Refactor configuration to use environment variables for SECRET_KEY and SOTUVCHI_KEY. Update DATABASE_URL handling for PostgreSQL and SQLite.

--- config.py
+++ config.py
@@ -11,21 +11,47 @@ PERMANENT_SESSION_LIFETIME = timedelta(days=30)
 
 class Config:
 
-    SECRET_KEY = "secret-key"
-
-    import os
-
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-
-    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace(
-        "postgres://",
-        "postgresql://",
-        1
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "secret-key"
     )
 
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+
+    # Render PostgreSQL
+    DATABASE_URL = os.environ.get(
+        "DATABASE_URL"
+    )
+
+
+    if DATABASE_URL:
+
+        if DATABASE_URL.startswith("postgres://"):
+
+            DATABASE_URL = DATABASE_URL.replace(
+                "postgres://",
+                "postgresql://",
+                1
+            )
+
+
+        SQLALCHEMY_DATABASE_URI = DATABASE_URL
+
+
+    else:
+
+        # Local kompyuter uchun SQLite
+        SQLALCHEMY_DATABASE_URI = (
+            "sqlite:///"
+            + os.path.join(
+                BASE_DIR,
+                "database.db"
+            )
+        )
+
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SOTUVCHI_KEY = os.getenv("SOTUVCHI_KEY")
+
+    SOTUVCHI_KEY = os.getenv(
+        "SOTUVCHI_KEY"
+    )
