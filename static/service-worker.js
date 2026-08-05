0d8a8203141a2ada83993c0a4eaaf02f37f430bf
Qodirbek <qodirbekpubg13@gmail.com>
2026-08-05 22:21:01 +0500

    Add PWA support

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,47 +1,31 @@
 const CACHE_NAME = "vendora-v1";
 
-
 const FILES = [
     "/",
-    "/static/css/style.css",
-    "/static/js/app.js",
-    "/static/manifest.json"
+    "/static/style.css",
+    "/static/js/app.js"
 ];
 
 
-self.addEventListener(
-"install",
-event => {
-
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-.then(cache => {
-
-return cache.addAll(FILES);
-
-})
-
-);
-
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll(FILES);
+        })
+    );
 });
 
 
-self.addEventListener(
-"fetch",
-event => {
+self.addEventListener("fetch", event => {
 
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
 
-event.respondWith(
+            return response || fetch(event.request);
 
-caches.match(event.request)
-.then(response => {
-
-return response || fetch(event.request);
-
-})
-
-);
-
+        })
+    );
 
 });
